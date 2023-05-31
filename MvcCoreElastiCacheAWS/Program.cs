0fb6f4e5f894808d2977a40c2b7159aad661effa
andreiggar <andreiggar@gmail.com>
2023-05-31 13:49:57 +0200

    Commit Distributed Cache

--- MvcCoreElastiCacheAWS/Program.cs
+++ MvcCoreElastiCacheAWS/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.Extensions.Options;
 using MvcCoreElastiCacheAWS.Repositories;
 using MvcCoreElastiCacheAWS.Services;
+using System.Diagnostics.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+options.Configuration = "cache-coches.4ymddt.ng.0001.use1.cache.amazonaws.com:6379";
+options.InstanceName = "ElastiCacheExample";
+});
+
 builder.Services.AddTransient<RepositoryCoches>();
 builder.Services.AddTransient<ServiceAWSCache>();
 builder.Services.AddControllersWithViews();
